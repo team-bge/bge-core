1650009c6f7e06423da4e78f7da02a830c4ad5b6
James King <james.king@facepunchstudios.com>
2023-02-04 14:39:42 +0100

    Added Hand.height

--- src/hand.ts
+++ src/hand.ts
@@ -54,16 +54,19 @@ export class Hand<TCard extends Card> extends LinearCardContainer<TCard> {
     readonly width: number;
 
     /**
+     * Height of the area that hand cards are displayed in, in centimeters. Matches the card height.
+     */
+    readonly height: number;
+
+    /**
      * How cards in a hand are positioned when there is free space.
      */
     readonly alignment: HandAlignment;
 
     override get footprint(): Footprint {
-        const dims = this.cardDimensions;
-
         return {
             width: this.width + 2,
-            height: dims.height + 2
+            height: this.height + 2
         };
     }
 
@@ -90,7 +93,11 @@ export class Hand<TCard extends Card> extends LinearCardContainer<TCard> {
     constructor(CardType: { new(...args: any[]): TCard }, width: number, options?: IHandOptions<TCard>) {
         super(CardType, LinearContainerKind.FirstInLastOut, options?.orientation, options?.autoSort);
     
+        const dims = Card.getDimensions(CardType);
+
         this.width = width;
+        this.height = dims.height;
+
         this.alignment = options?.alignment ?? HandAlignment.Center;
     }
 
@@ -103,7 +110,7 @@ export class Hand<TCard extends Card> extends LinearCardContainer<TCard> {
             prompt: ctx.player?.prompt.get(this),
 
             width: this.width,
-            height: dims.height,
+            height: this.height,
 
             cards: [],
 
