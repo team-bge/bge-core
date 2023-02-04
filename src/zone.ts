81bc6ca71c28886f98d16039bd53f3af4ce79217
James King <james.king@facepunchstudios.com>
2023-02-04 14:10:36 +0100

    Documentation for Zone

--- src/zone.ts
+++ src/zone.ts
@@ -3,13 +3,37 @@ import { DisplayContainer } from "./displaycontainer.js";
 import { Footprint, GameObject } from "./object.js";
 import { IView, OutlineStyle, ViewType, ZoneView } from "./views.js";
 
+/**
+ * @summary Represents a rectangular region on the table, with an outline and optional label.
+ * @description Child objects can be dynamically added or removed with `Zone.children`, or in
+ * a deriving class with `@display()` annotated properties.
+ */
 export class Zone extends GameObject {
+    /**
+     * Width of the zone in centimeters.
+     */
     width: number;
+
+    /**
+     * Height of the zone in centimeters.
+     */
     height: number;
 
+    /**
+     * Appearance of the outline around this zone.
+     */
     outlineStyle: OutlineStyle = OutlineStyle.Dashed;
+
+    /**
+     * Optional label text to describe this zone to players.
+     */
     label?: string;
 
+    /**
+     * @summary Contains child objects that are displayed inside this zone.
+     * @description This will also contain objects from `@display()` annotated properties,
+     * using the property keys as names.
+     */
     readonly children = new DisplayContainer();
 
     override get footprint(): Footprint {
@@ -19,6 +43,11 @@ export class Zone extends GameObject {
         };
     }
     
+    /**
+     * Represents a rectangular region on the table, with an outline and optional label.
+     * @param width Width of the zone in centimeters.
+     * @param height Height of the zone in centimeters.
+     */
     constructor(width = 10, height = 10) {
         super();
 
